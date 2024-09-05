577d3ab1f5cfc3c5eb7ac85247a2ce18408b8dc5
A-Dylite2 <143554342+A-Dylite2@users.noreply.github.com>
2024-09-05 17:31:04 -0400

    Update temp_converter.py
    Newer changes.

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,1 +1,3 @@
 My name is A-Dylite. Quite a Dylite if I may add!
+I stream on Twitch and YouTube on Mondays, Wednesday, Fridays, and the first and third Saturday of each month!
+Always remember that you matter! :)

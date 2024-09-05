ba91f36a729ee7e4121f678e97d159a9938aa0e8
A-Dylite2 <143554342+A-Dylite2@users.noreply.github.com>
2024-09-05 17:40:51 -0400

    Update temp_converter.py
    Even Newer Changes.

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+def c2f(c):
+    return c * 9 / 5 + 32
+def main(cel):
+    return c2f(cel)
+if __name__ == "__main__":
+    cel = 100         # input
+    print(main(cel))  # output
+
 My name is A-Dylite. Quite a Dylite if I may add!
 I stream on Twitch and YouTube on Mondays, Wednesday, Fridays, and the first and third Saturday of each month!
 Always remember that you matter! :)

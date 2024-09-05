0f6c658ef74fd72ad2eaeb016b73366a0e17d36e
A-Dylite2 <143554342+A-Dylite2@users.noreply.github.com>
2024-09-05 17:29:26 -0400

    Update temp_converter.py
    New Changes.

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,12 +1,1 @@
-#!/usr/bin/python3
-
-# process
-def c2f(c):
-    return c * 9 / 5 + 32
-
-def main(cel):
-    return c2f(cel)
-
-if __name__ == "__main__":
-    cel = 100         # input
-    print(main(cel))  # output
+My name is A-Dylite. Quite a Dylite if I may add!
